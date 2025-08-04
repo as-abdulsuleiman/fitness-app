4a38dec21dac717d2fa26095c8e28bf4302604b0
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2025-08-04 17:56:32 +0530

    chore: minor changes

--- FitnessApp/Home/Views/HomeView.swift
+++ FitnessApp/Home/Views/HomeView.swift
@@ -117,7 +117,6 @@ struct HomeView: View {
                             WorkoutCard(workout: workout)
                         }
                     }
-                    
                     .padding(.bottom)
                     
                 }
