4bae8bb01c5b3eba02f0fc7863fc1db4409367f0
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2025-08-04 11:07:23 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Abdul Suleiman on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
